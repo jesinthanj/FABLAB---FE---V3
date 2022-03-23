6d6f0115d5bf0a7eca95d45e8d4e92689c7578d4
sneha0110 <you@example.com>
2022-03-23 11:34:53 +0530

    main page updated

--- src/pages/admin/MainPage.tsx
+++ src/pages/admin/MainPage.tsx
@@ -1,91 +1,97 @@
 import React from "react";
 import { Card } from "@mui/material";
 import machine from "../../assets/machine1.jpg";
-
+import Layout from "../../components/Layout";
 export default function MainPage() {
   return (
     <>
-      <div
-        className="container"
-        style={{ maxHeight: "100vh", minWidth: "100vw" }}
-      >
-        <p
-          className="m-5 justify-content-center d-flex"
-          style={{ fontWeight: "bold", fontSize: "20px" }}
+      <Layout>
+        <div
+          className="container"
+          style={{
+            maxHeight: "100vh",
+            minWidth: "100vw",
+            marginTop: "20%",
+          }}
         >
-          Welcome to LICET FABLAB
-        </p>
-        <div className="justify-content-center align-items-center min-vh-100">
-          <div className="justify-content-center align-items-center d-flex m-5">
-            <Card
-              variant="outlined"
-              style={{
-                height: "270px",
-                width: "250px",
-                background: "#e6e6e6",
-                borderRadius: "10px",
-              }}
-            >
-              <div className="justify-content-center d-flex">
-                <img
-                  style={{ marginTop: "30px", borderRadius: "10px" }}
-                  width={200}
-                  height={170}
-                  src={machine}
-                  alt=""
-                />
-              </div>
-              <div className="justify-content-center d-flex m-3">
-                <button
-                  className="m-2"
-                  type="button"
-                  style={{
-                    background: "#FF8E23",
-                    border: "#FF8E23",
-                    borderRadius: "3px",
-                  }}
-                >
-                  Add Slots
-                </button>
-              </div>
-            </Card>
-          </div>
-          <div className="justify-content-center d-flex m-5">
-            <Card
-              variant="outlined"
-              style={{
-                height: "270px",
-                width: "250px",
-                background: "#e6e6e6",
-                borderRadius: "10px",
-              }}
-            >
-              <div className="justify-content-center d-flex">
-                <img
-                  style={{ marginTop: "30px", borderRadius: "10px" }}
-                  width={200}
-                  height={170}
-                  src={machine}
-                  alt=""
-                />
-              </div>
-              <div className="justify-content-center d-flex m-3">
-                <button
-                  className="m-2"
-                  type="button"
-                  style={{
-                    background: "#FF8E23",
-                    border: "#FF8E23",
-                    borderRadius: "3px",
-                  }}
-                >
-                  Add Equipments
-                </button>
-              </div>
-            </Card>
+          <p
+            className="m-5 justify-content-center d-flex"
+            style={{ fontWeight: "bold", fontSize: "20px" }}
+          >
+            Welcome to LICET FABLAB
+          </p>
+          <div className="justify-content-center align-items-center min-vh-100">
+            <div className="justify-content-center align-items-center d-flex m-5">
+              <Card
+                variant="outlined"
+                style={{
+                  height: "270px",
+                  width: "250px",
+                  background: "#e6e6e6",
+                  borderRadius: "10px",
+                }}
+              >
+                <div className="justify-content-center d-flex">
+                  <img
+                    style={{ marginTop: "30px", borderRadius: "10px" }}
+                    width={200}
+                    height={170}
+                    src={machine}
+                    alt=""
+                  />
+                </div>
+                <div className="justify-content-center d-flex m-3">
+                  <button
+                    className="m-2"
+                    type="button"
+                    style={{
+                      background: "#FF8E23",
+                      border: "#FF8E23",
+                      borderRadius: "3px",
+                    }}
+                  >
+                    Add Slots
+                  </button>
+                </div>
+              </Card>
+            </div>
+            <div className="justify-content-center d-flex m-5">
+              <Card
+                variant="outlined"
+                style={{
+                  height: "270px",
+                  width: "250px",
+                  background: "#e6e6e6",
+                  borderRadius: "10px",
+                }}
+              >
+                <div className="justify-content-center d-flex">
+                  <img
+                    style={{ marginTop: "30px", borderRadius: "10px" }}
+                    width={200}
+                    height={170}
+                    src={machine}
+                    alt=""
+                  />
+                </div>
+                <div className="justify-content-center d-flex m-3">
+                  <button
+                    className="m-2"
+                    type="button"
+                    style={{
+                      background: "#FF8E23",
+                      border: "#FF8E23",
+                      borderRadius: "3px",
+                    }}
+                  >
+                    Add Equipments
+                  </button>
+                </div>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
+      </Layout>
     </>
   );
 }
