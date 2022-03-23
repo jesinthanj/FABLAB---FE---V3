ce278fbb9c9b1e67a07792129f5edcde69f61198
Elizabeth Subhiksha <esubivictoria129@gmail.com>
2022-03-23 11:33:50 +0530

    Home Page Layout done

--- src/pages/users/HomePage.tsx
+++ src/pages/users/HomePage.tsx
@@ -1,9 +1,111 @@
+import * as React from "react";
 import Layout from "../../components/Layout";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import printing from "../../assets/equipment/3d.png";
+import Vinyl from "../../assets/equipment/vinyl.png";
+import CNC from "../../assets/equipment/cnc.png";
+
+const details = [
+  {
+    equipname: "CNC Router",
+    amt: "Rs 200",
+    image: CNC,
+  },
+  {
+    equipname: "Pruning Plants",
+    amt: "Rs 200",
+    image: Vinyl,
+  },
+  {
+    equipname: "Pruning Plants",
+    amt: "Rs 200",
+    image: printing,
+  },
+];
 
 export default function HomePage() {
   return (
-    <div>
-      <Layout />
+    <div className="vh-100 align-items-center justify-content-center">
+      <h5
+        className="pb-1 mx-2 py-5 d-flex justify-content-center"
+        style={{
+          fontFamily: "montserrat",
+          fontWeight: "bold",
+          letterSpacing: "5px",
+        }}
+      >
+        Pick your slots for
+      </h5>
+      {details &&
+        details.map((items, index) => {
+          return (
+            <div key={index}>
+              <div className="align-items-center justify-content-center">
+                {" "}
+                <Card
+                  sx={{
+                    maxWidth: 335,
+                    borderBottomLeftRadius: "20px",
+                    borderBottomRightRadius: "20px",
+                    borderTopLeftRadius: "20px",
+                    borderTopRightRadius: "20px",
+                    backgroundColor: "#F5F5F5",
+                    margin: "20px",
+                    boxShadow: "5px 5px 5px #888888",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    height="194"
+                    image={items.image}
+                    alt="Paella dish"
+                    style={{
+                      borderBottomLeftRadius: "20px",
+                      borderBottomRightRadius: "20px",
+                    }}
+                  />
+                  <CardContent>
+                    <div className="d-flex justify-content-between">
+                      <Typography
+                        className="pb-1 d-flex justify-content-center"
+                        style={{
+                          fontFamily: "montserrat",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        {items.equipname}
+                      </Typography>
+                      <Button
+                        variant="contained"
+                        className="rounded-3"
+                        href="#contained-buttons"
+                        sx={{
+                          borderRadius: 5,
+                          backgroundColor: "#FFA500",
+                        }}
+                      >
+                        Book Now
+                      </Button>
+                    </div>
+                    <Typography
+                      className="pb-1 d-flex justify-content-start"
+                      style={{
+                        fontFamily: "montserrat",
+                      }}
+                      color="text.secondary"
+                    >
+                      {items.amt}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
