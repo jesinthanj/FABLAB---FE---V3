b781c39cd66d380b3d6cd70c4ce17233de58e3fa
veroni-d20 <veronishwetha.23it@licet.ac.in>
2022-03-23 15:11:11 +0530

    confirmation page for slots

--- src/pages/admin/ConfirmationPage.tsx
+++ src/pages/admin/ConfirmationPage.tsx
@@ -1,5 +1,33 @@
 import React from "react";
+import Layout from "../../components/Layout";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export default function ConfirmationPage() {
-  return <div>ConfirmationPage</div>;
+  let navigate = useNavigate();
+  return (
+    <Layout>
+      <div className="vh-100 d-flex align-items-center justify-content-center flex-column">
+        <p>The Slot is added</p>
+        <h2 className="my-2">Succesfully!</h2>
+        <div className="text-center">
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "#FFA73F",
+              width: 100,
+              mt: 2,
+              borderRadius: 10,
+              ":hover": { backgroundColor: "#ff623f" },
+            }}
+            onClick={() => {
+              navigate("/mainPage");
+            }}
+          >
+            Home
+          </Button>
+        </div>
+      </div>
+    </Layout>
+  );
 }
