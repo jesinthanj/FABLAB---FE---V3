19494405c7bd3addab30ed0a8f479b898e318cad
Jesinthan0703 <jesinthan.23cs@licet.ac.in>
2022-03-23 10:03:29 +0530

    logo added

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,10 +1,22 @@
 import Layout from "../../components/Layout";
 import TextField from "@mui/material/TextField";
+import LicetLogo from "../../assets/licet_logo.png";
 
 export default function Login() {
   return (
     <Layout>
       <div className="vh-100 d-flex align-items-center justify-content-center">
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            paddingRight: "8px",
+            paddingTop: "8px",
+          }}
+        >
+          <img src={LicetLogo} alt="" width="80" height="80" />
+        </div>
         <div className="card shadow">
           <div className="card-body">
             <h5
@@ -27,14 +39,20 @@ export default function Login() {
             >
               FABLAB
             </h1>
-            <TextField
-              className="py-3 d-flex justify-content-center"
-              label="username"
-            />
-            <TextField
-              className="py-3 d-flex justify-content-center"
-              label="password"
-            />
+            <div>
+              <TextField
+                className="py-3 d-flex justify-content-center"
+                label="username"
+              />
+              <TextField
+                className="py-3 d-flex justify-content-center"
+                label="password"
+              />
+            </div>
+            <div className="d-flex justify-content-between">
+              <p style={{ fontSize: "10px" }}>Forgot password</p>
+              <p style={{ fontSize: "10px" }}>New user?</p>
+            </div>
           </div>
         </div>
       </div>
