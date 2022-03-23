ecd1aa301f7c39fcd89febd3fde875bae67064fa
dhivya910 <dhivyadharshni910@gmail.com>
2022-03-23 15:30:02 +0530

    changes done

--- src/pages/auth/ChangePwd.tsx
+++ src/pages/auth/ChangePwd.tsx
@@ -7,61 +7,65 @@ import Menu from "../../components/Menu";
 export default function ChangePwd() {
   return (
     <Layout>
-      <Menu/>
-      <div className="vh-100 d-flex align-items-center justify-content-center">
       <Box
         sx={{
-          my: 5,
+          my: 15,
           mx: 4,
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
         }}
       >
-      <Box component="h1" sx={{ letterSpacing: 3, m: 1 , justifyContent:"center"}}>
-        CHANGE
-        {/* PASSWORD */}
+        <div className="text-center">
+          <Box
+            component="h1"
+            sx={{
+              letterSpacing: 3,
+              m: 1,
+              justifyContent: "center",
+              fontSize: "25px",
+              fontWeight: "bold",
+            }}
+          >
+            CHANGE <br /> PASSWORD
+          </Box>
+        </div>
+
+        <TextField
+          margin="normal"
+          fullWidth
+          id="password"
+          label="New Password"
+          name="password"
+          autoComplete="password"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          name="cpassword"
+          label="Confirm Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <Button
+          type="button"
+          variant="contained"
+          size="medium"
+          sx={{
+            mt: 5,
+            mb: 2,
+            backgroundColor: "#FF8E23",
+            alignItems: "center",
+            justifyContent: "center",
+            // width: "50%",
+            borderRadius: "8px",
+          }}
+        >
+          Submit
+        </Button>
       </Box>
-      <Box component="h1" sx={{ letterSpacing: 3, m: 1 }}>
-        PASSWORD
-      </Box>
-      <Box component="form" noValidate sx={{ mt: 1, width: "25%" }} />
-      <TextField
-        margin="normal"
-        fullWidth
-        id="password"
-        label="New Password"
-        name="password"
-        autoComplete="password"
-        autoFocus
-      />
-      <TextField
-        margin="normal"
-        fullWidth
-        name="cpassword"
-        label="Confirm Password"
-        type="password"
-        id="password"
-        autoComplete="current-password"
-      />
-      <Button
-        type="button"
-        variant="contained"
-        size="medium"
-        sx={{
-          mt: 5,
-          mb: 2,
-          backgroundColor: "#FF8E23",
-          alignItems: "center",
-          justifyContent: "center",
-          // width: "50%",
-          borderRadius: "8px"
-        }}
-      >
-        Submit
-      </Button>
-      </Box>
-      </div>
     </Layout>
   );
 }
--- src/pages/auth/ResetPwd.tsx
+++ src/pages/auth/ResetPwd.tsx
@@ -1,71 +1,86 @@
-import React from 'react';
+import React from "react";
 import Layout from "../../components/Layout";
-import {Button, TextField} from '@mui/material';
-import { Typography, Box} from '@mui/material';
-import Menu from '../../components/Menu';
+import { Button, TextField } from "@mui/material";
+import { Typography, Box } from "@mui/material";
+import Menu from "../../components/Menu";
 
 export default function ResetPwd() {
   return (
     <Layout>
+      <Box
+        sx={{
+          my: 15,
+          mx: 4,
+          display: "flex",
+          flexDirection: "column",
+          //alignItems: "center",
+        }}
+      >
+        <div className="text-center">
           <Box
+            component="h1"
             sx={{
-              my: 15,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              letterSpacing: 3,
+              m: 3,
+              justifyContent: "center",
+              fontSize: "25px",
+              fontWeight: "bold",
             }}
           >
-            <Box
-              component="h1"
-              sx={{ letterSpacing: 3, m: 1}}
-            >
-              RESET
-            </Box>
-            <Box
-              component="h1"
-              sx={{ letterSpacing: 3, m: 1}}
-            >
-              PASSWORD
-            </Box>
-              <TextField
-                
-                margin="normal"
-                fullWidth
-                id="email"
-                label="Email"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <Button
-                  type="button"
-                  variant="contained"
-                  size="small"
-                  sx={{ mb: 2, backgroundColor: '#FF8E23', alignItems: 'right', borderRadius:"10px"}}
-                >
-                 Generate OTP
-                </Button>
-              <TextField
-                
-                margin="normal"
-                fullWidth
-                name="password"
-                label="OTP"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-                <Button
-                  type="button"
-                  variant="contained"
-                  size="medium"
-                  sx={{ mt: 5, mb: 2, backgroundColor: '#FF8E23', alignItems: 'center', justifyContent: 'center',borderRadius: "8px"}}
-                  href="/changepwd"
-                >
-                 Next
-                </Button>
-                </Box>
+            RESET <br />
+            PASSWORD
+          </Box>
+        </div>
+
+        <TextField
+          margin="normal"
+          fullWidth
+          id="email"
+          label="Email"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+
+        <div className="text-end">
+          <Button
+            type="button"
+            variant="contained"
+            size="small"
+            sx={{
+              mb: 2,
+              backgroundColor: "#FF8E23",
+            }}
+          >
+            Generate OTP
+          </Button>
+        </div>
+        <TextField
+          margin="normal"
+          fullWidth
+          name="password"
+          label="OTP"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <div className="text-center">
+          <Button
+            type="button"
+            variant="contained"
+            size="medium"
+            sx={{
+              my: 3,
+              backgroundColor: "#FF8E23",
+              borderRadius: "8px",
+              width: "30%",
+            }}
+            href="/changepwd"
+          >
+            Next
+          </Button>
+        </div>
+      </Box>
     </Layout>
   );
 }
